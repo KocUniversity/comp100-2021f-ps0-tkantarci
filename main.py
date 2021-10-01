7da1c84438c7c5de8d903abfe72c146791b0e6bc
tkantarci <91739135+tkantarci@users.noreply.github.com>
2021-10-01 13:45:15 +0000

    1. User inputs two strings, assigned to the variables x and y
    2. x and y are turned into integers
    
    3. Calculates x to the power of y and prints it
    
    4. Calculates base 2 logarithm of x and prints it
    
    5. Prints my student ID

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy
+def function():
+  x = input("Enter number x: ")
+  y = input("Enter number y: ")
+  x = int(x)
+  y = int(y)
+  exp = x**y
+  print(exp)
+  log = numpy.log2(x)
+  print(log)
+  my_id = 79957
+  print(my_id)
+
+function()
